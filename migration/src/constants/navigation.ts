041defa9318cb7caa65893d064354abf69dfc0fc
erikhambaryan <36689029+erikhambaryan@users.noreply.github.com>
2024-12-27 13:35:47 +0400

    Delete migration directory

--- migration/src/constants/navigation.ts
+++
@@ -1,7 +0,0 @@
-export const navigationItems = [
-  { id: 'services', label: 'ՄԵՐ ՄԱՍԻՆ' },
-  { id: 'about', label: 'ԱՌԱՔԵԼՈՒԹՅՈՒՆ' },
-  { id: 'our-services', label: 'ԾԱՌԱՅՈՒԹՅՈՒՆՆԵՐ' },
-  { id: 'experts', label: 'ԱԲՓ' },
-  { id: 'contact', label: 'ԿԱՊ' },
-] as const;
--- migration/src/types/language.ts
+++
@@ -1,5 +0,0 @@
-export enum Language {
-  Armenian = 'am',
-  Russian = 'ru',
-  English = 'en',
-}
