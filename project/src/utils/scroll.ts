e00520af4c38f056d01c445a4b8e55da636b0358
erikhambaryan <36689029+erikhambaryan@users.noreply.github.com>
2024-12-27 13:35:33 +0400

    Delete project directory

--- project/src/utils/scroll.ts
+++
@@ -1,6 +0,0 @@
-export const scrollToElement = (elementId: string) => {
-  const element = document.getElementById(elementId);
-  if (element) {
-    element.scrollIntoView({ behavior: 'smooth' });
-  }
-};
